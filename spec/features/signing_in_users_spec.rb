1b3d17df9b59343d8231cd7dc2c0d20328f0cf13
Andre Hoong <hoongandre@gmail.com>
2020-10-13 14:18:38 -0700

    fixed dashboard

--- spec/features/signing_in_users_spec.rb
+++ spec/features/signing_in_users_spec.rb
@@ -11,7 +11,20 @@ RSpec.feature 'Siging users in' do
         fill_in 'Email', with: @john.email
         fill_in 'Password', with: @john.password
         click_button 'Log in'
-
         expect(page).to have_content('Signed in successfully.')
+        expect(page).not_to have_link('Sign Up')
+        expect(page).not_to have_link('Sign In')
+    end
+
+    scenario '- sign in with invalid credentials' do
+        visit '/'
+        click_link 'Sign In'
+        fill_in 'Email', with: 'demo2@example.com'
+        fill_in 'Password', with: 'password'
+        click_button 'Log in'
+        expect(page).to have_content('Invalid Email or password.')
+        expect(page).to have_link('Sign Up')
+        expect(page).to have_link('Sign In')
+        expect(page).not_to have_link('Sign Out')
     end
 end
