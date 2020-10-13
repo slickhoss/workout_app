05904c81cd057848fe498ad50806008b75ef212f
Andre Hoong <hoongandre@gmail.com>
2020-10-13 15:37:56 -0700

    Create exercise unsuccessfully spec

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,6 @@
 class Exercise < ApplicationRecord
+  validates :duration_in_min, numericality: { greater_than: 0}
+  validates :workout, presence: true
+  validates :workout_date, presence: true
   belongs_to :user
 end
